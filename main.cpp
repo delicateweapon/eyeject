cdc53da63df6c0661c98b39e10ab2e2c572e7600
delicateweapon <macole343@icloud.com>
2024-11-21 10:42:14 +0000

    cleanup

--- main.cpp
+++ main.cpp
@@ -2,76 +2,9 @@
 #include "Camera.hpp"
 #include "Garden.hpp"
 #include "constants.hpp"
-
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <ncurses.h>
-#endif
+#include "Console.hpp"
 
 #include <thread>
-
-void *console_func(void *args)
-{
-    bool quit = false;
-    App::Camera *Cam0;
-    char output[256];
-
-#ifdef _WIN32
-
-    while (!quit) {
-        Cam0 = App::Camera::selected;
-
-        sprintf(output,
-            "Camera: Cam0\n"
-            "Position: (.x = %f, .y = %f)\n"
-            "Look Angle: %f radians\n"
-            "FOV: %f radians\n",
-            Cam0->position.x, Cam0->position.y, Cam0->look_angle, Cam0->fov);
-
-        if (GetAsyncKeyState('Q')) {
-            quit = true;
-        }
-
-        Sleep(100);
-    }
-
-#else
-
-    initscr();
-    timeout(100);
-
-    int ch;
-
-    while (!quit) {
-        clear();
-
-        Cam0 = App::Camera::selected;
-
-        sprintf(output,
-            "Camera: Cam0\n"
-            "Position: (.x = %f, .y = %f)\n"
-            "Look Angle: %f radians\n"
-            "FOV: %f radians\n",
-            Cam0->position.x, Cam0->position.y, Cam0->look_angle, Cam0->fov);
-
-        printw("%s", output);
-
-        ch = getch();
-        switch (ch) {
-        case 'Q' - 'A' + 1:
-            quit = true;
-            break;
-        default:
-            break;
-        }
-    }
-
-    endwin();
-
-#endif
-    return nullptr;
-}
 
 int main()
 {
